92ab2d8137678de5408c0150d8b5b031de724bdc
Aarav Maloo <aaravmaloov@gmail.com>
2025-01-31 18:08:18 +0530

    added GUI changes

--- app/connectdb.py
+++ app/connectdb.py
@@ -5,10 +5,12 @@ import mysql.connector
 from PyQt6.QtWidgets import *
 from PyQt6.QtGui import QPixmap, QImage
 from PyQt6.QtCore import Qt
+from adodbapi import connect
+
 dotenv.load_dotenv("password_sql_server.env")
 mysql_password = os.getenv("MYSQL_PASSWORD")
 
-class front_end_ui(QMainWindow):
+class login_page(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Grades")
@@ -16,69 +18,50 @@ class front_end_ui(QMainWindow):
         self.resize(1400, 800)
 
 
+        layout = QVBoxLayout()
 
-# class UpdatorConnector:
-#     def __init__(self):
-#         pass
-#
-#     def register(self):
-#         try:
-#
-#             connectdb = mysql.connector.connect(
-#                 host="192.168.1.38",
-#                 user="aaravmalooking",
-#                 password=mysql_password
-#             )
-#             print("register your school and be a part of the best grading system ever!")
-#             print("To register, please type your school name and a database specifically for your school will be "
-#                   "created!")
-#             school_name = input(colorama.Fore.MAGENTA+"$> ")
-#             dbcursor = connectdb.cursor()
-#             dbcursor.execute(f"CREATE DATABASE IF NOT EXISTS {school_name}")
-#             print("To protect the database, you need to set up a root's account.")
-#             rootacc_usrname = str(input("[$]usrname_root> "))
-#             rootacc_usrpass = str(input("[$]usrname_rootpass> "))
-#             rootacc_usrname_lower = rootacc_usrname.lower()
-#             try:
-#                 connectdb_updated = mysql.connector.connect(
-#                     host="192.168.1.38",
-#                     user="aaravmalooking",
-#                     password=mysql_password,
-#                     database=f"{school_name}"
-#                 )
-#                 cursor_updated = connectdb_updated.cursor()
-#                 create_table_query = """
-#                 CREATE TABLE IF NOT EXISTS teachers (
-#                     usrname VARCHAR(100),
-#                     usrpass VARCHAR(100) UNIQUE,
-#                     usrpriv VARCHAR(100)
-#                 );
-#                 """
-#                 cursor_updated.execute(create_table_query)
-#                 insert_query = """
-#                 INSERT INTO teachers (usrname, usrpass, usrpriv)
-#                 VALUES (%s, %s, %s)
-#                 """
-#                 data_teachers= (f"{rootacc_usrname_lower}", f"{rootacc_usrpass}", "ALL")
-#                 cursor_updated.execute(insert_query, data_teachers)
-#             except mysql.connector.Error as e:
-#                 print(f"Something went wrong {e}")
-#
-#
-#         except mysql.connector.Error as e:
-#             print(colorama.Fore.RED + f"Something went wrong: {e}" + colorama.Style.RESET_ALL)
-#
-#
-#         except Exception as e:
-#             print(colorama.Fore.RED + f"Unexpected error: {e}" + colorama.Style.RESET_ALL)
+        hidden_top = QPushButton("Top")
+        layout.addWidget(hidden_top)
 
+
+        signup = QPushButton("Login")
+        layout.addWidget(signup)
+        signup.setGeometry(640, 320, 200, 50)
+        signup.setStyleSheet(
+            "background-color: #FFFFFF; color: black; font: 14pt Homemade Apple; border-radius: 146px;"
+        )
+        # You can connect the button to a function if needed
+        # signup.clicked.connect(self.stop_capture)
+
+        # Create the "Register" button and add it to the layout
+        register = QPushButton("Register")
+        layout.addWidget(register)
+
+        # Hide the "Top" button
+        hidden_top.hide()
+
+        # Set the layout to the central widget of the QMainWindow
+        central_widget = QWidget(self)
+        central_widget.setLayout(layout)
+        self.setCentralWidget(central_widget)
+
+# class backend:
+#     connect_db = mysql.connector.connect(
+#         host="192.168.1.38",
+#         user="aaravmalooking",
+#         password=mysql_password
+#     )
+#     cursor_0 = connect_db.cursor()
 #
-#
-# instance = UpdatorConnector()
-# instance.register()
+
+
+
+
+
+
 
 if __name__ == "__main__":
     app = QApplication([])
-    window = front_end_ui()
+    window = login_page()
     window.show()
     app.exec()
